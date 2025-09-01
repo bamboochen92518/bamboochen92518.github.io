728460dcba0de4c01cd11962107125594fdfaeee
bamboo_chen <b10902005@ntu.edu.tw>
2025-09-01 23:36:06 +0800

    init

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+async function loadPartial(id, file) {
+  const response = await fetch(file);
+  const content = await response.text();
+  document.getElementById(id).innerHTML = content;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadPartial("header", "partials/header.html");
+  loadPartial("footer", "partials/footer.html");
+});
+
